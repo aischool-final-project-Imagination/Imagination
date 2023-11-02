001991772e277a6992497ac74c6dab82fcf76b9c
이치연 <leechi@ichiyeons-MacBook-Air.local>
2023-11-02 20:43:44 +0900

    style : mypage UI & member information change modal UI

--- src/apps/front/src/features/auth/mypage/Mypage.tsx
+++ src/apps/front/src/features/auth/mypage/Mypage.tsx
@@ -1,7 +1,48 @@
 import React from 'react';
+import EditModal from './components/EditModal';
 
 const Mypage = () => {
-  return <div>mypage</div>;
+  return (
+  <div>
+  <EditModal></EditModal>
+  <div className='page'>
+    <nav className='nav'>nav</nav>
+    <div className='mypage'>    
+      <header className='mypage__header'>
+          <section className='mypage__background'></section>
+          <section className='mypage__profile'>
+            <div className='mypage__profile__photo'></div>
+            <button className='mypage__edit'>
+              <img src="edit.png"  alt="" />
+            </button>
+          </section>
+          <section className='mypage__info'>
+            <div className='mypage__info__header'>
+              <h1 className='mypage__username'>leechi</h1>
+              <span className='mypage__desc'>나는 최고다! </span>
+            </div>
+            <div className='mypage__info__footer'>
+              <div className='mypage__info--post'>
+                <span>6</span>
+                <span>Posts</span>
+              </div>
+              <div className='mypage__info--follow'>
+                <span>232</span>
+                <span>Followers</span>
+              </div>
+              <div className='mypage__info--follower'>
+                <span>828</span>
+                <span>Following</span>
+              </div>
+            </div>
+          </section>
+        </header>
+        <footer className='mypage__footer'>
+        </footer>
+      </div>
+  </div>
+  </div>
+        )
 };
 
 export default Mypage;
---
+++ src/apps/front/src/features/auth/mypage/components/EditModal.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const EditModal = () => {
+  return (
+    <div className='edit__modal'>
+        <section className='edit__modal__box'>
+            <div className='edit__modal__background'></div>
+            <div className='edit__modal__white'>
+                <div className='edit__modal__photo'></div>
+                <input type="file" className='edit__modal__upload--hidden' />
+                <div className='edit__modal__upload'>
+                    <img src="add.png" alt="" />
+                    <span>프로필 이미지</span>
+                </div>
+            </div>
+            <div className='edit__modal__inputs'>
+                <input type="text" placeholder='이름 변경' />
+                <input type="text" placeholder='상태메시지 변경하기' />
+            </div>
+            <div className='edit__modal__footer'>
+                <button className='edit__modal__reset'>초기화하기</button>
+                <div>
+                    <button className='edit__modal__cancel'>취소하기</button>
+                    <button className='edit__modal__change'>정보변경</button>
+                </div>
+            </div>
+        </section>
+    </div>
+  )
+}
+
+export default EditModal
