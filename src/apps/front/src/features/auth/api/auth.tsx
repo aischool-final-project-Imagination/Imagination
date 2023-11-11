6fa3b8701a0859bcf58c73772b729818026ccda2
이치연 <leechi@ichiyeons-MacBook-Air.local>
2023-11-11 23:14:30 +0900

    feat: Add baseURL PORT

--- src/apps/front/src/features/auth/api/auth.tsx
+++ src/apps/front/src/features/auth/api/auth.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
-
+import { PORT } from '../../../../../../packages/models/port';
 const instance = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: `http://localhost:${PORT}`,
   headers: {
     accept: 'application/json',
   },
