e6a6b22e0aac83b7aec5b87df96718a8bc91e332
이치연 <leechi@ichiyeons-MacBook-Air.local>
2023-11-13 13:46:23 +0900

    feat(login): Add userData using jotai

--- src/apps/front/src/features/auth/login/components/Login.tsx
+++ src/apps/front/src/features/auth/login/components/Login.tsx
@@ -3,11 +3,14 @@ import { useEffect, useState } from 'react';
 import axios from '../../api/auth';
 import { LoginForm } from './LoginForm';
 import { LoginQuestionBox } from './LoginQuestionBox';
+import { atom, useAtom } from 'jotai';
+
+const userAtom = atom({});
 
 const Login = () => {
-  const [isLogin, setIsLogin] = useState(false);
-  const [user, setUser] = useState({});
+  const [user, setUser] = useAtom(userAtom);
   console.log(user);
+  const [isLogin, setIsLogin] = useState(false);
 
   useEffect(() => {
     axios.get('/login/success').then((res) => {
